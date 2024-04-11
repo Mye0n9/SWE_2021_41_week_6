0c1751379058ffacd13a7d8394de71fb721887b8
Mye0n9 <rlaaudtjq0201@naver.com>
2024-04-11 18:59:07 +0900

    made with return the list

--- main.py
+++ main.py
@@ -9,13 +9,32 @@ def twoSum(nums: List[int], target: int) -> List[int]:
             tmp.append(i)
     
     for i in range(len(tmp)):
-        for j in range(i,len(tmp)):
+        for j in range(i+1,len(tmp)):
             if(nums[i]+nums[j] == target):
-                res: List[int] = [i,j]
-                print(f"Output: {res}")
-                print(f"Explanation: Because nums[{i}] + nums[{j}] == {target}, we return {res}")
-                return 0
+                res: List[int] = [i,j,nums[i],nums[j]]
+                return res
     return 0
 
 if '__main__':
-    twoSum([2,7,11,15],9)
+    nums = [2,7,11,15]
+    target = 9
+    res:List[int] = twoSum(nums,target)
+    
+    print(f"Output: {res[:2]}")
+    print(f"Explanation: Because nums[{res[2]}] + nums[{res[3]}] == {target}, we return {res[:2]}")
+    
+    nums = [3,2,4]
+    target = 6
+    res:List[int] = twoSum(nums,target)
+    
+    print(f"Output: {res[:2]}")
+    print(f"Explanation: Because nums[{res[2]}] + nums[{res[3]}] == {target}, we return {res[:2]}")
+    
+    nums = [3,3]
+    target = 6
+    res:List[int] = twoSum(nums,target)
+    
+    print(f"Output: {res[:2]}")
+    print(f"Explanation: Because nums[{res[2]}] + nums[{res[3]}] == {target}, we return {res[:2]}")
+    
+    
